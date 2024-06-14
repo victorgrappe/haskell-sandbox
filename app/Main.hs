e06b3da6559fcfba6f6b1df2733055f72f842479
victorgrappe <victor.grappe@protonmail.com>
2024-06-14 23:57:49 +0200

    My first real hello world in haskell

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,10 @@ module Main (main) where
 import Lib
 
 main :: IO ()
-main = print (f x y)
+main = do
+  putStrLn "Welcome into my first Haskell program"
+  print (f x y)
+  putStrLn "That was cool"
 
 -- main = someFunc
 -- main = print (yoInt 1 2)
