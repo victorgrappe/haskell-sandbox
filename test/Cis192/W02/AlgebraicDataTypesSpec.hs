3b49fd103b7736ca82a8149a3892edd0e1ec3065
victorgrappe <victor.grappe@protonmail.com>
2024-06-26 22:55:32 +0200

    use hspec-discover to split tests into file corresponding to src

--- test/Cis192/W02/AlgebraicDataTypesSpec.hs
+++ test/Cis192/W02/AlgebraicDataTypesSpec.hs
@@ -1,18 +1,16 @@
+module W02.AlgebraicDataTypesSpec (spec) where
+
 import Cis192.W02.AlgebraicDataTypes
--- import Control.Exception (evaluate)
--- import Lib
 import Test.Hspec
 
--- import Test.QuickCheck
-
-main :: IO ()
-main = hspec $ do
+spec :: Spec
+spec = do
   describe "AlgebraicDataTypes" $ do
     it "Test CIS 192 - Week 02" $ do
       isSmall shoe `shouldBe` True
       isSmall2 shoe `shouldBe` True
-      -- shoe `shouldBe` (listO'Things !! 0)
-      -- shoe `shouldBe` (Shoe :: Thing)
       yo !! 0 `shouldBe` (1 :: Integer)
       (yo !! 0) `shouldBe` 1
-      (listO'Things !! 0) `shouldBe` (shoe :: Thing)
+      -- shoe `shouldBe` (listO'Things !! 0)
+      -- shoe `shouldBe` (Shoe :: Thing)
+      "I'll be back" `shouldBe` "I'll be back"
